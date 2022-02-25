4f12b39a948da820fb7824df3df268a4d2e986bc
gezhiwei <gezhiwei8899@gmail.com>
2022-02-25 17:56:32 +0800

    add Modules

---
+++ java-native-test/src/main/java/com/gezhiwei/syncmap/MainTest.java
@@ -0,0 +1,13 @@
+package com.gezhiwei.syncmap;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainTest {
+
+
+    public Map<Integer, Integer> map = Collections.synchronizedMap(new HashMap<>());
+
+
+}
